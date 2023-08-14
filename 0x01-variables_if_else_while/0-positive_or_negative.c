d2accfdfb0577463d22c9241b594c83859283d9b
Opeyemi Dada <opeyemiolusola@gmail.com>
2023-08-14 11:59:33 -0700

    writing my first C file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,20 +1,21 @@
-#include <stdlid.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Alway 0 (Success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-	if (n == 0)
+if (n == 0)
 printf("%d is zero\n", n);
 else if (n < 0)
 printf("%d is negative\n", n);
