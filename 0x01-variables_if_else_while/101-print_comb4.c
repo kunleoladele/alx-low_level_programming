fa318605b506d24ba2630adba30454dba2cd2626
Opeyemi Dada <opeyemiolusola@gmail.com>
2023-08-14 18:11:35 -0700

    program that prints all possible combinations of two 2 digit no

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include  <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always o (Success)
+ */
+
+int main(void)
+{
+
+int hundreds;
+int tens;
+int ones;
+
+for (hundreds = 0; hundreds <= 9; hundreds++)
+{
+for (tens = hundreds + 1; tens <= 9; tens++)
+{
+for (ones = tens + 1; ones <= 9; ones++)
+{
+putchar(hundreds + '0');
+putchar(tens + '0');
+putchar(ones + '0');
+
+if (hundreds < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+
+}
