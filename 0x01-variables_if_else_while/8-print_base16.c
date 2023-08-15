9938bd249b8314f9d79663d13b369abb7514ba22
Opeyemi Dada <opeyemiolusola@gmail.com>
2023-08-14 18:00:02 -0700

    the 9th c file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,15 +5,18 @@
  * 
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int num;
-	char alphabet;
+int num;
+char alphabet;
 
-	for (num = 48; num < 58; num++)
-		putchar(num);
-	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
-		putchar(alphabet);
-	putchar('\n');
-	return (0);
+for (num = 48; num < 58; num++)
+putchar(num);
+
+for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+putchar(alphabet);
+putchar('\n');
+
+return (0);
 }
