5be970300795c98ae36697166d01b7be68eadfcc
Opeyemi Dada <opeyemiolusola@gmail.com>
2023-08-14 17:47:50 -0700

    the 9th c file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char num;
+
+for (num = '0'; num <= '9'; num++)
+putchar(digit);
+
+for (num = 'a'; num <= 'f'; num++)
+putchar(num);
+
+putchar('\n');
+
+return (0);
+
+}
