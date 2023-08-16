848f9f9ab14105ca1f92a11db527a59f575295fe
Opeyemi Dada <opeyemiolusola@gmail.com>
2023-08-16 03:04:04 -0700

    fourth nested file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - check if character input "a" is lowercase if YES display 1
+ * another case display 0
+ *
+ * c: character in lowercase
+ *
+ * Return: 1 for lowercase, otherwise return 0
+ */
+int _islower(int c)
+{
+int c;
+if (c >= "a" && c <= a)
+{
+return(1)
+}
+else
+{return(0)
+}
+
+{
+_putchar('\n');
+}
