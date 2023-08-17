3895a45bba5f46221fba63724803517f20ca4d0a
Opeyemi Dada <opeyemiolusola@gmail.com>
2023-08-16 18:18:21 -0700

    debugging for the first time

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c)
+void print_remaining_days(int day, int month, int year)
+int convert(int month, int day)
 
 #endif /* MAIN_H */
