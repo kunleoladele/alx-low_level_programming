7c681d11b6f09ec198799a74cca4202a99f2b019
Opeyemi Dada <opeyemiolusola@gmail.com>
2023-08-18 09:11:39 -0700

    third function n nested loop test

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ * @a: First interger
+ * @b: Second integer
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+int x;
+
+x = a * b;
+return (x);
+}
