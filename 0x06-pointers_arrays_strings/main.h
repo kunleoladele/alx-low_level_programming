ae9c163cc6de047ec2870bf6e446f4b3e8335485
Opeyemi Dada <opeyemiolusola@gmail.com>
2023-08-23 15:56:57 -0700

    function changing all lowercase of a string to uppercase

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,6 +12,8 @@ void reverse_array(int *a, int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+char *string_toupper(char *);
 int _strcmp(char *s1, char *s2);
+
 
 #endif /*MAIN_H*/
