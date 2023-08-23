64372ff40dc852f87b7dd4d502f998f3dc265c64
Opeyemi Dada <opeyemiolusola@gmail.com>
2023-08-23 10:42:21 -0700

    first commit

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+ */
+
+void reverse_array(int *a, int n);
+char *_strcat(char *dest, char *src);
+
+#endif /*MAIN_H*/
