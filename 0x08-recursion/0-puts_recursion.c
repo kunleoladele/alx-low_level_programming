3a235a792bfeb94cc2c0457d0ea114f9ef2eefdb
kunleoladele <opeyemiolusola2004@gmail.com>
2023-09-18 09:25:28 -0700

    function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,14 @@
 *@s:character to be printed
 *Return:returns void
 */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		_putchar('\n');
-	else
+	if (*s)
 	{
-		_putchar(s[0]);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	else
+	_putchar('\n');
 }
